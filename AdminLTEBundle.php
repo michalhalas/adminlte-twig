5eea42351533edb052c281e30dc36c46d2e0ab01
Michał Hałas <michal.halas@epicode.net>
2019-02-20 14:05:11 +0100

    base

---
+++ AdminLTEBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MH\AdminLTE;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Michał Hałas <michal.halas@epicode.net>
+ */
+class AdminLTEBundle extends Bundle{
+
+}
