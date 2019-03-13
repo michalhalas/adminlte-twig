03e363358cf635cc87cc76941a91b2befc5798da
Michał Hałas <michal.halas@epicode.net>
2019-03-13 15:26:20 +0100

    bundle działa, szkoda że sam się nie ładuje do bundles.php

--- AdminLTEBundle.php
+++ AdminLTEBundle.php
@@ -2,11 +2,19 @@
 
 namespace MH\AdminLTE;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * @author Michał Hałas <michal.halas@epicode.net>
  */
-class AdminLTEBundle extends Bundle{
-
+class AdminLTEBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
